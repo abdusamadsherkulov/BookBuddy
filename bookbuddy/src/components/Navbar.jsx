22dcbc364e51e8898bbdc63c5cdff485a7f3b0f3
abdusamadsherkulov <abdusamadsherkulov@gmail.com>
2025-07-01 14:34:04 +0500

    icons and logos added, navbar fixed

--- bookbuddy/src/components/Navbar.jsx
+++ bookbuddy/src/components/Navbar.jsx
@@ -1,26 +1,69 @@
 import {Link} from 'react-router-dom';
-import bookWormLogo from '../assets/BookWorm_light.svg';
+import UniLinkLogo from '../assets/UniLink_light.svg';
+import HomeIconLight from '../assets/HomeIcon_light.svg';
+import SearchIconLight from '../assets/SearchIcon_light.svg';
+import MesssageIconLight from '../assets/MessageIcon_light.svg';
+import NotificationIconLight from '../assets/NotificationIcon_light.svg';
+import NotificationIconNewLight from '../assets/NotificationIconNew_light.svg';
+import CreateIconLight from '../assets/CreateIcon_light.svg';
+import ProfileIconLight from '../assets/ProfileIcon_light.svg';
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <a href="#">
-        <img src={bookWormLogo} alt="logo" id="logo" />
+        <img src={UniLinkLogo} alt="logo" id="logo" />
       </a>
       <div className="nav-menu">
         <ul>
-          <li>
-            <a href="App.jsx">Home</a>
-          </li>
-          <li>
-            <a href="AddBook.jsx">Add Book</a>
-          </li>
-          <li>
-            <a href="Favorites.jsx">Favorites</a>
-          </li>
-          <li>
-            <a href="Quotes.jsx">Quotes</a>
-          </li>
+          <a href="#">
+            <li>
+              <img className="icon_l" src={HomeIconLight} alt="Home" />
+              &nbsp;&nbsp;Home
+            </li>
+          </a>
+          <a href="#">
+            <li>
+              <img className="icon_l" src={SearchIconLight} alt="Search" />
+              &nbsp;&nbsp;Search connection
+            </li>
+          </a>
+          <a href="#">
+            <li>
+              <img className="icon_l" src={MesssageIconLight} alt="Message" />
+              &nbsp;&nbsp;Messages
+            </li>
+          </a>
+          <a href="#">
+            <li>
+              <img
+                className="icon_m"
+                src={NotificationIconLight}
+                alt="notification"
+              />
+              &nbsp;&nbsp;Notification
+            </li>
+          </a>
+          <a href="#">
+            <li>
+              <img
+                className="icon_m"
+                src={CreateIconLight}
+                alt="notification"
+              />
+              &nbsp;&nbsp;Create
+            </li>
+          </a>
+          <a href="#">
+            <li>
+              <img
+                className="icon_s"
+                src={ProfileIconLight}
+                alt="notification"
+              />
+              &nbsp;&nbsp;Profile
+            </li>
+          </a>
         </ul>
       </div>
     </div>
